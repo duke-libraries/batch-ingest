ee77676628e299daa4714d4e261c944b1cfecddd
Jim Coble <jim.coble@duke.edu>
2012-11-08 16:42:17 -0500

    correct typos

--- VIC/020collectionfoxmlcreate.sh
+++ VIC/020collectionfoxmlcreate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 java -jar /srv/fedora-working/saxon/saxon9he.jar \
 	-s:/srv/fedora-working/ingest/VIC/collection/master/master.xml \
-	-xsl:/srv/fedora-working/xslt/ingest/ObjectFoxmlCreate.xsl \
+	-xsl:/srv/fedora-working/ingest/bin/xslt/ObjectFoxmlCreate.xsl \
 	-o:/srv/fedora-working/ingest/VIC/log/collection-foxml-create.xml \
-        foxmlFilePath=/srv/fedora-working/ingest/VIC/collection/foxml/ \        
+        foxmlFilePath="/srv/fedora-working/ingest/VIC/collection/foxml/" \
         label="Vica Nazi Propaganda Comics Collection"
