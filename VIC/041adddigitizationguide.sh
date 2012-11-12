0e3da2638c46aeccbe2279cf40f6a08f3f4bbd9d
Jim Coble <jim.coble@duke.edu>
2012-11-12 14:51:24 -0500

    work in progress

--- VIC/041adddigitizationguide.sh
+++ VIC/041adddigitizationguide.sh
@@ -2,7 +2,7 @@
 java -jar /srv/fedora-working/saxon/saxon9he.jar \
 	-s:/srv/fedora-working/ingest/VIC/collection/master/master-with-pids.xml \
 	-xsl:/srv/fedora-working/ingest/bin/xslt/ObjectAddModifyDatastream.xsl \
-	-o:/srv/fedora-working/ingest/VIC/logs/collection-add-modify-datastream.xml \
+	-o:/srv/fedora-working/ingest/VIC/collection/modify/addDigitizationGuideDS.xml \
         dsLocationPath="/srv/fedora-working/ingest/VIC/collection/digitizationGuide/" \
         dsFilenameElement="digitizationguideFilename" \
         dsId="digitizationGuide" \
