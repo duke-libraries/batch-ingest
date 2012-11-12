3db05da2bec5d384db83a06cdb1cac0be85f4410
Jim Coble <jim.coble@duke.edu>
2012-11-12 16:47:25 -0500

    update path and file names

--- VIC/050collectionaddrelsext.sh
+++ VIC/050collectionaddrelsext.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 java -jar /srv/fedora-working/saxon/saxon9he.jar \
-	-s:/srv/fedora-working/ingest/VIC/collection/master/collection-master-with-pids.xml \
-	-xsl:/srv/fedora-working/xslt/ingest/RelsExtFromPidMaster.xsl \
-	-o:/srv/fedora-working/ingest/VIC/collection/modify/modify.xml \
+	-s:/srv/fedora-working/ingest/VIC/collection/master/master-with-pids.xml \
+	-xsl:/srv/fedora-working/ingest/bin/xslt/RelsExtFromPidMaster.xsl \
+	-o:/srv/fedora-working/ingest/VIC/collection/modify/addRelsExtDS.xml \
         contentModels="afmodel:Collection" \
         adminPolicy="duke-apo:vica"
