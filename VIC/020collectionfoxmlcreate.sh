d9dee552058ea3b2504b2b46fc42aa2aaf1cdb8e
Jim Coble <jim.coble@duke.edu>
2012-11-12 13:22:43 -0500

    correct path for log file

--- VIC/020collectionfoxmlcreate.sh
+++ VIC/020collectionfoxmlcreate.sh
@@ -2,6 +2,6 @@
 java -jar /srv/fedora-working/saxon/saxon9he.jar \
 	-s:/srv/fedora-working/ingest/VIC/collection/master/master.xml \
 	-xsl:/srv/fedora-working/ingest/bin/xslt/ObjectFoxmlCreate.xsl \
-	-o:/srv/fedora-working/ingest/VIC/log/collection-foxml-create.xml \
+	-o:/srv/fedora-working/ingest/VIC/logs/collection-foxml-create.xml \
         foxmlFilePath="/srv/fedora-working/ingest/VIC/collection/foxml/" \
         label="Vica Nazi Propaganda Comics Collection"
