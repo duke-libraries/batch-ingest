48a0cc5c9bf12d94f546fe5c00cd84a0068a486d
Jim Coble <jim.coble@duke.edu>
2012-11-12 16:45:36 -0500

    add admin policy

--- VIC/050collectionaddrelsext.sh
+++ VIC/050collectionaddrelsext.sh
@@ -3,4 +3,5 @@ java -jar /srv/fedora-working/saxon/saxon9he.jar \
 	-s:/srv/fedora-working/ingest/VIC/collection/master/collection-master-with-pids.xml \
 	-xsl:/srv/fedora-working/xslt/ingest/RelsExtFromPidMaster.xsl \
 	-o:/srv/fedora-working/ingest/VIC/collection/modify/modify.xml \
-        contentModels="afmodel:Collection"
+        contentModels="afmodel:Collection" \
+        adminPolicy="duke-apo:vica"
