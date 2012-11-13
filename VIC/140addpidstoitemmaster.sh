b363d46b8b1f1f14f6d8a61f2d2f4ab48686455d
Jim Coble <jim.coble@duke.edu>
2012-11-13 09:16:00 -0500

    continue item refactoring

--- VIC/140addpidstoitemmaster.sh
+++ VIC/140addpidstoitemmaster.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 java -jar /srv/fedora-working/saxon/saxon9he.jar \
-	-s:/srv/fedora-working/ingest/VIC/item/master/item-master.xml \
-	-xsl:/srv/fedora-working/xslt/ingest/AddPids2Master.xsl \
-	-o:/srv/fedora-working/ingest/VIC/item/master/item-master-with-pids.xml \
+	-s:/srv/fedora-working/ingest/VIC/item/master/master.xml \
+	-xsl:/srv/fedora-working/ingest/bin/xslt/AddPids2Master.xsl \
+	-o:/srv/fedora-working/ingest/VIC/item/master/master-with-pids.xml \
         collectioncode=VIC \
         objecttype=item
